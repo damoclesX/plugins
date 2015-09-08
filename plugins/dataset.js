facab5e7c63d3f589419bca73bdcd4b24f984093
damoclesX <785541602@qq.com>
2015-09-08 10:35:51 +0800

    增加注释

--- plugins/dataset.js
+++ plugins/dataset.js
@@ -1,31 +1,47 @@
+/*
+    html元素值的获取和设置
+    new Dataset() 初始化
+        @selector string 选择器,只操作这个元素下面的元素 不依赖jquery,使用document.querySelector
+        @attr string 属性名 默认为html元素的data-field属性值
+*/
 define(function(){
-    function DataSet(selector){
+    //定义构造函数
+    function DataSet(selector,attr){
         if(document.querySelectorAll){
             this.el = document.querySelector(selector)
+            this.attr = attr || 'data-field';
         }
     }
+    //获取html元素的值
     DataSet.prototype.get = function(string){
+        //获取单个值
         if(typeof string == 'string'){
-            var el = this.el.querySelector('[data-field="'+string+'"]');
+            var el = this.el.querySelector('['+this.attr+'="'+string+'"]');
             return get_value(el);
+        //获取所有值
         }else{
             var res = {};
-            [].slice.call(this.el.querySelectorAll('[data-field]')).forEach(function(e,i){
-                res[e.getAttribute('data-field')] = get_value(e);
+            var _this = this;
+            [].slice.call(this.el.querySelectorAll('['+this.attr+']')).forEach(function(e,i){
+                res[e.getAttribute(_this.attr)] = get_value(e);
             })
            return res;
         }
     }
+    //设置html元素的值
     DataSet.prototype.set = function(key,value){
+        //设置单个值
         if(arguments.length == 2){
-            set_value(this.el.querySelector('[data-field="'+key+'"]'),value||'');
+            set_value(this.el.querySelector('['+this.attr+'="'+key+'"]'),value||'');
+        //设置多个值
         }else{
-            [].slice.call(this.el.querySelectorAll('[data-field]')).forEach(function(e,i){
-                set_value(e,key[e.getAttribute('data-field')] || '')
+            var _this = this;
+            [].slice.call(this.el.querySelectorAll('['+this.attr+']')).forEach(function(e,i){
+                set_value(e,key[e.getAttribute(_this.attr)] || '')
             })
         }
     }
-    //获取值
+    //获取html值
     function get_value(ele){
         var tagName = ele.tagName.toLowerCase();
         var value = '';
@@ -43,7 +59,7 @@ define(function(){
         }
         return value;
     }
-    //设置值
+    //设置html值
     function set_value(ele,value){
         var tagName = ele.tagName.toLowerCase();
         switch(tagName){
