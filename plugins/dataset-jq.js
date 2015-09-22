d612f8b3d7e75327a5a811fea62c91f649de14ae
damoclesX <785541602@qq.com>
2015-09-22 13:49:26 +0800

    增加注释

--- plugins/dataset-jq.js
+++ plugins/dataset-jq.js
@@ -1,35 +1,45 @@
 define(['jQuery'],function($){
-    function DataSet(selector){
+    function DataSet(selector,attr){
         this.$el = $(selector);
-        this.$fields = this.$el.find('[data-field]');
+        attr = attr || 'data-field';
+        this.$fields = this.$el.find('['+attr+']');
         this.fields = {};
         var _this  = this;
+        //缓存元素
         this.$fields.each(function(i,e){
             _this.fields[e.dataset.field] = $(e);
         })
     }
+    //获取html元素的值
     DataSet.prototype.get = function(string){
+        //获取单个值
         if(typeof string == 'string'){
             var $el = this.fields[string];
             return _value($el);
+        //获取多个值
         }else{
             var res = {};
-           this.$fields.each(function(i,e){
+            this.$fields.each(function(i,e){
                 res[e.dataset.field] = _value($(e));
-           })
-           return res;
+            })
+            return res;
         }
     }
+    //设置html元素的值
     DataSet.prototype.set = function(key,value){
+        //设置单个值
         if(arguments.length == 2){
             $value(this.fields[key],value||'');
+        //设置多个值
         }else{
             this.$fields.each(function(i,e){
                 key[e.dataset.field] && $value($(e),key[e.dataset.field] || '')
             })
         }
     }
+    //清空html元素的值
     DataSet.prototype.clear = function(except){
+        //排除不需要情况的值
         var bExcept = !!except;
         this.$fields.each(function(i,e){
             if(bExcept){
@@ -48,6 +58,7 @@ define(['jQuery'],function($){
             }
         })
     }
+    //根据字段名获取单个元素
     DataSet.prototype.el = function(field){
         if(typeof field == 'string'){
             return this.fields[field];
